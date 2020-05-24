ec03d57ac0b2d56dfa66d6eaaa5fae5e63ec12f3
Fábio <fsmaiorano@gmail.com>
2020-05-24 10:24:39 +0100

    implement 2 tests to authenticatedservice

--- gobarber-backend/src/modules/users/services/AuthenticatedUserServicer.spec.ts
+++ gobarber-backend/src/modules/users/services/AuthenticatedUserServicer.spec.ts
@@ -1,4 +1,5 @@
 import 'reflect-metadata';
+import AppError from '@shared/errors/AppError';
 import AuthenticateUserService from './AuthenticateUserService';
 import FakeUsersRepository from '../repositories/fakes/FakeUsersRepository';
 import CreateUserService from './CreateUserService';
@@ -18,5 +19,28 @@ describe('AuthenticateUser', () => {
 
     expect(response).toHaveProperty('token');
     expect(response.user).toEqual(user);
+  });
+
+  it('should not be able to authenticate with none existing user', async () => {
+    const fakeUsersRepository = new FakeUsersRepository();
+    const fakeHashProvider = new FakeHashProvider();
+    const authenticateUser = new AuthenticateUserService(fakeUsersRepository, fakeHashProvider);
+
+    expect(authenticateUser.execute({ email: 'johndoe@example.com', password: '123456' })).rejects.toBeInstanceOf(
+      AppError,
+    );
+  });
+
+  it('should not be able to authenticate with wrong password', async () => {
+    const fakeUsersRepository = new FakeUsersRepository();
+    const fakeHashProvider = new FakeHashProvider();
+    const createUser = new CreateUserService(fakeUsersRepository, fakeHashProvider);
+    const authenticateUser = new AuthenticateUserService(fakeUsersRepository, fakeHashProvider);
+
+    await createUser.execute({ name: 'John Doe', email: 'johndoe@example.com', password: '123456' });
+
+    expect(authenticateUser.execute({ email: 'johndoe@example.com', password: '123' })).rejects.toBeInstanceOf(
+      AppError,
+    );
   });
 });
