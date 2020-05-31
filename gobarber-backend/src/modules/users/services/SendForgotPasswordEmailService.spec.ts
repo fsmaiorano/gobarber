0e701b1a42d8ffe2bb9841b85bd57944c9be4f27
Fábio <fsmaiorano@gmail.com>
2020-05-31 10:35:04 +0100

    checkpoint tests

--- gobarber-backend/src/modules/users/services/SendForgotPasswordEmailService.spec.ts
+++ gobarber-backend/src/modules/users/services/SendForgotPasswordEmailService.spec.ts
@@ -1,16 +1,32 @@
 import 'reflect-metadata';
 import FakeMailProvider from '@shared/container/providers/MailProvider/fakes/FakeMailProvider';
+import AppError from '@shared/errors/AppError';
 import FakeUsersRepository from '../repositories/fakes/FakeUsersRepository';
 import SendForgotPasswordEmailService from './SendForgotPasswordEmailService';
+import FakeUserTokensRepository from '../repositories/fakes/FakeUserTokensRepository';
+
+let fakeMailProvider: FakeMailProvider;
+let fakeUsersRepository: FakeUsersRepository;
+let sendForgotPasswordEmailService: SendForgotPasswordEmailService;
+let fakeUserTokenRepository: FakeUserTokensRepository;
 
 describe('SendForgotPasswordEmailService', () => {
+  beforeEach(() => {
+    fakeUsersRepository = new FakeUsersRepository();
+    fakeMailProvider = new FakeMailProvider();
+    fakeUsersRepository = new FakeUsersRepository();
+    fakeMailProvider = new FakeMailProvider();
+    fakeUserTokenRepository = new FakeUserTokensRepository();
+
+    sendForgotPasswordEmailService = new SendForgotPasswordEmailService(
+      fakeUsersRepository,
+      fakeMailProvider,
+      fakeUserTokenRepository,
+    );
+  });
+
   it('Should be able to recover the password using a valid e-mail', async () => {
-    const fakeUsersRepository = new FakeUsersRepository();
-    const fakeMailProvider = new FakeMailProvider();
-
     const sendEmail = jest.spyOn(fakeMailProvider, 'sendMail');
-
-    const sendForgotPasswordEmailService = new SendForgotPasswordEmailService(fakeUsersRepository, fakeMailProvider);
 
     await fakeUsersRepository.create({
       name: 'John Doe',
@@ -25,7 +41,27 @@ describe('SendForgotPasswordEmailService', () => {
     expect(sendEmail).toHaveBeenCalled();
   });
 
-  it('Should not be able to recover the password using a invalid e-mail', async () => {});
+  it('Should not be able to recover the password using a invalid e-mail', async () => {
+    await expect(
+      sendForgotPasswordEmailService.execute({
+        email: 'broken@email.com',
+      }),
+    ).rejects.toBeInstanceOf(AppError);
+  });
 
-  it('Should generate a forgot password token', async () => {});
+  it('Should generate a forgot password token', async () => {
+    const generateToken = jest.spyOn(fakeUserTokenRepository, 'generate');
+
+    const user = await fakeUsersRepository.create({
+      name: 'John Doe',
+      email: 'johndoe@example.com',
+      password: '123456',
+    });
+
+    await sendForgotPasswordEmailService.execute({
+      email: 'johndoe@example.com',
+    });
+
+    expect(generateToken).toHaveBeenCalledWith(user.id);
+  });
 });
--- gobarber-backend/src/modules/users/services/SendForgotPasswordEmailService.ts
+++ gobarber-backend/src/modules/users/services/SendForgotPasswordEmailService.ts
@@ -2,6 +2,8 @@ import { inject, injectable } from 'tsyringe';
 //import AppError from '@shared/errors/AppError';
 import IUsersRepository from '@modules/users/repositories/IUsersRepository';
 import IMailProvider from '@shared/container/providers/MailProvider/models/IMailProvider';
+import AppError from '@shared/errors/AppError';
+import IUserTokensRepository from '../repositories/IUserTokensRepository';
 //import User from '../infra/typeorm/entities/User';
 
 interface IRequest {
@@ -13,9 +15,18 @@ class SendForgotPasswordEmailService {
   constructor(
     @inject('UsersRepository') private usersRepository: IUsersRepository,
     @inject('MailProvider') private mailProvider: IMailProvider,
+    @inject('UserTokensRepository') private userTokensRepository: IUserTokensRepository,
   ) {}
 
   public async execute({ email }: IRequest): Promise<void> {
+    const user = await this.usersRepository.findByEmail(email);
+
+    if (!user) {
+      throw new AppError('User does not exists');
+    }
+
+    await this.userTokensRepository.generate(user.id);
+
     this.mailProvider.sendMail(email, 'Password Recovery');
   }
 }
