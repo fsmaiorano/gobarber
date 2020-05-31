e54841890cb0a72871ac0a150839fe827a0484dd
Fábio <fsmaiorano@gmail.com>
2020-05-31 12:42:55 +0100

    create usertoken migration

--- gobarber-backend/src/shared/container/index.ts
+++ gobarber-backend/src/shared/container/index.ts
@@ -6,11 +6,12 @@ import './providers';
 import UsersRepository from '@modules/users/infra/typeorm/repositories/UsersRepository';
 import IUsersRepository from '@modules/users/repositories/IUsersRepository';
 
-// import IUserTokensRepository from '@modules/users/repositories/IUserTokensRepository';
-// import UserTokensRepository from '@modules/users/infra/typeorm/repositories/UsersRepository';
+import IUserTokensRepository from '@modules/users/repositories/IUserTokensRepository';
+import UserTokensRepository from '@modules/users/infra/typeorm/repositories/UserTokensRepository';
 
 import IAppointmentRepository from '@modules/appointments/repositories/IAppointmentsRepository';
 import AppointmentsRepository from '@modules/appointments/infra/typeorm/repositories/AppointmentsRepository';
 
 container.registerSingleton<IUsersRepository>('UsersRepository', UsersRepository);
+container.registerSingleton<IUserTokensRepository>('UserTokensRepository', UserTokensRepository);
 container.registerSingleton<IAppointmentRepository>('AppointmentsRepository', AppointmentsRepository);
