bcf1751adf671294cbefd8a97f72f0a61a47cf9e
Fábio <fsmaiorano@gmail.com>
2020-06-06 11:55:56 +0100

    implement mongodb and notification service

--- gobarber-backend/src/modules/appointments/infra/http/controllers/ProviderAppointmentsController.ts
+++ gobarber-backend/src/modules/appointments/infra/http/controllers/ProviderAppointmentsController.ts
@@ -6,12 +6,9 @@ export default class ProviderAppointmentsController {
   public async index(request: Request, response: Response): Promise<Response> {
     const provider_id = request.user.id;
     const { day, month, year } = request.body;
-    console.log('aaa');
     const listProvidersAppointmentsService = container.resolve(ListProviderAppointmentsService);
 
     const appointment = await listProvidersAppointmentsService.execute({ provider_id, day, month, year });
-    console.log('teste');
-    console.log(appointment);
     return response.json(appointment);
   }
 }
