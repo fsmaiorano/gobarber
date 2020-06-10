dbe3dd793c06d314c28bb889f12451d6c0a0ee5b
Fábio <fsmaiorano@gmail.com>
2020-06-10 08:09:05 +0100

    refactory

--- gobarber-backend/src/modules/appointments/services/ListProviderAppointmentsService.ts
+++ gobarber-backend/src/modules/appointments/services/ListProviderAppointmentsService.ts
@@ -1,5 +1,6 @@
 import { injectable, inject } from 'tsyringe';
 import ICacheProvider from '@shared/container/providers/CacheProvider/models/ICacheProvider';
+import { classToClass } from 'class-transformer';
 import IAppointmentRepository from '../repositories/IAppointmentsRepository';
 import Appointment from '../infra/typeorm/entities/Appointment';
 
@@ -29,7 +30,7 @@ class ListProviderAppointmentsService {
         year,
       });
 
-      await this.cacheProvider.save(cacheKey, appointments);
+      await this.cacheProvider.save(cacheKey, classToClass(appointments));
     }
 
     return appointments;
